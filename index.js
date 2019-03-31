1555cb6ccf3d5916e9dcfdfc2a3acdf79fae3ead
Conal Cosgrove <conal.cosgrove@salesforce.com>
2019-04-01 00:36:10 +0100

    Updated for queue create

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const app = require('express')();
 const request = require('superagent');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 app.get('/switches', (req, res) => {
     request.get('http://192.168.56.3:8080/v1.0/topology/switches')
     .then((resp) => {
@@ -11,7 +14,7 @@ app.get('/switches', (req, res) => {
 app.get('/qos/queue/*', (req, res) => {
     console.log(req.originalUrl);
 
-    switchId = req.originalUrl.split('/queue/')[1];
+    const switchId = req.originalUrl.split('/queue/')[1];
     console.log('Getting queue info for switch' ,switchId);
     const url = `http://192.168.56.3:8080/qos/queue/${switchId}`;
     console.log(url);
@@ -24,6 +27,27 @@ app.get('/qos/queue/*', (req, res) => {
         console.log('Failed to get queues', err.response.text);
     });
 });
+
+app.post('/qos/queue/*', (req, res) => {
+    const switchId = req.originalUrl.split('/queue/')[1];
+    console.log('Adding queue to switch', switchId);
+    console.log(req.body);
+    request.post(`http://192.168.56.3:8080/qos/queue/${switchId}`)
+    .send(req.body)
+    .then((resp) => {
+        res.header('Access-Control-Allow-Origin','http://localhost:3000').header('Access-Control-Allow-Methods', 'POST').header('Access-Control-Allow-Headers','Content-Type').send(resp);
+    })
+    .catch((err) => {
+        res.header('Access-Control-Allow-Origin','http://localhost:3000').header('Access-Control-Allow-Methods', 'POST').header('Access-Control-Allow-Headers','Content-Type').status(400).send(err);
+        console.log('Failed to add queue', err);
+    });
+
+});
+
+app.options('/qos/queue/*', (req, res) => {
+    res.header('Access-Control-Allow-Methods', 'POST').header('Access-Control-Allow-Headers','Content-Type').header('Access-Control-Allow-Origin','http://localhost:3000').send('hi');
+});
+
 
 app.get('/qos/rules/*', (req, res) => {
     console.log(req.originalUrl);
